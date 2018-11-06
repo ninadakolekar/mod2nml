2ad412e7c117648c160ed3296bafcac155ded3ea
ninadakolekar <ninad.akolekar@gmail.com>
2018-11-06 18:23:16 +0530

    Add check for valid block names

--- src/lexer.py
+++ src/lexer.py
@@ -9,3 +9,35 @@ import util
 
 pp = pprint.PrettyPrinter(depth=4)
 
+def lexer(modFile):
+
+    blocks = {}
+    data = {}
+
+    modFileData = [(str(line.strip())).replace('\t',' ') for line in open(modFile)]
+
+    currentLineNumber = 0
+
+    while(currentLineNumber<len(modFileData)):
+        
+        line = modFileData[currentLineNumber]
+
+        if len(line)>0:
+
+            # Title of the mod file
+            if line.startswith('TITLE'):
+                
+                blocks['TITLE'] = line[6:].strip()
+            
+            if '{' in line:
+                braceIndex = lines.index('{')
+                blockHeading = lines[:braceIndex].strip()
+
+                # Check if block heading is valid
+                if util.isValidBlock(blockHeading):
+                    
+                    # Initialize empty list to store the parameters specified in this block
+                    blocks[blockHeading] = []
+
+
+
--- src/util.py
+++ src/util.py
@@ -6,3 +6,15 @@ def checkBraces(inputString, braceCounter):
             elif c=='}':
                 braceCounter-=1
     return braceCounter
+
+def feedData(templateFile,data):
+    with open(templateFile) as f:
+        feeder = airspeed.Template(f.read())
+    return feeder.merge(data)
+
+def isValidBlock(string blockHeading):
+    blocks = ['TITLE','UNITS','NEURON','PARAMETER','ASSIGNED','STATE','PROCEDURE','DERIVATIVE','BREAKPOINT','INITIAL','UNITSOFF','UNITSON']
+    if blockHeading in blocks:
+        return True
+    else:
+        return False
