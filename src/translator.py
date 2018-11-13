9b5392f67d0e0769c67161f9c131cd938b14030c
ninadakolekar <ninad.akolekar@gmail.com>
2018-11-13 23:23:47 +0530

    Fixed bugs, added template, translator working now

--- src/translator.py
+++ src/translator.py
@@ -1,3 +1,7 @@
+from util import feedData
+from lexer import lexer
+
+
 # Call as  gate['instances'] = str(countInstances(breakpointBlock,state))
 def countInstances(br,ch):
   count = 0
@@ -10,3 +14,12 @@ def countInstances(br,ch):
         count+=1
   return count
 
+def translatorWrapper(data,template,outFilename):
+  output = open(outFilename,"w")
+  output.write(feedData(template,data))
+
+def translator(dataFile):
+  translatorWrapper(lexer(dataFile,False),"template.nml","output.nml")
+  
+if __name__=="__main__":
+    translator("../examples/mod/KCa_Channel.mod")
