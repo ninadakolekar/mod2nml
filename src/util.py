8fdcce85b550148f4ed2cbb5b3ef3427f7886153
ankur singh <es16btech11003@iith.ac.in>
2018-11-28 09:37:33 +0530

    Function to find equation type and extracting gate info

--- src/util.py
+++ src/util.py
@@ -27,24 +27,68 @@ def isValidBlock(blockHeading):
 
 def equationParser(expr):
     flag = 0
-    string1 = re.compile('[(]*\d+[)]*[*][e][x][p][(]*[A-Za-z]+[-]\d+[)]*[/][(]*\d+[)]*')
-    
-    if (len(string1.findall(expr)) != 0 and string1.findall(expr)[0] == True):
-        print('exponential')
+    string1 = re.compile('[(]*[-]?[\d+][.][\d+][)]*[*][e][x][p][(]*[A-Za-z]+[+-][-]?[\d+][.][\d+][)]*[/][-]?[\d+][.][\d+][)]*')
+    if(len(string1.findall(expr))!=0 and string1.findall(expr)[0] == expr):
         flag = 1
     else:
-        string1 = re.compile('[(]*\d+[)]*\s[/]\s[(]*[e][x][p][(]*\w+\s[-]\d+[)]*[/]\s\d+[)]*')
-    if(string1.search(expr) is not None):
-        print('sigmoid')
-        flag = 2
-    else:
-        string1 = re.compile('[(]*\d+[)]*[*][(]*\w+\s[-]\s\d+[)]*[/][(]*\d+[)]*[/][(]*[1]\s[-]\s[e][x][p][(]*\w+\s[-]\s\d+[)]*[/][(]*\d+[)]*')
-        if (string1.search(expr)!=None):
-            flag = 3
+        string1=re.compile('[(]*[-]?\d+[.][\d+][)]*[/][(]*[e][x][p][(]*[A-Za-z]+[+-][-]?\d+[.]\d+[)]*[/][(]*[-]?\d+[.]\d+[)]*[+][1][)]*')
+        if(len(string1.findall(expr))!= 0 and string1.findall(expr)[0] == expr):
+            flag = 2
         else:
-            print('generic')
-            flag = 4
-
+            string1=re.compile('[(]*[-]?\d+[.]\d+[)]*[*][(]*[A-Za-z]+[-][-]?\d+[.]\d+[)]*[/][(]*[-]?\d+[.]\d+[)]*[/][(]*[1][-][e][x][p][(]*[-][(]*[A-Za-z]+[-][-]?\d+[.]\d+[)]*[/][(]*[-]?\d+[.]\d+[)]*')
+            if(len(string1.findall(expr))!= 0 and string1.findall(expr)[0] == expr):
+                flag = 3
+            else:
+                flag = 4
+                print('generic')
+    a = {}
+    if(flag == 1):
+        string3=re.compile('[A-Za-z]+[-](\d+[.]\d+)')
+        y=string3.findall(expr)
+        string2=re.compile('[-]?\d+[.]\d+')
+        x=string2.findall(expr)
+        a['type']="exponential"
+        a['rate']  = x[0]
+        if(len(y) == 0):
+            a['midpoint']=x[1]
+        else:
+            a['midpoint']=y[0]
+        a['scale']=x[2]
+    
+    if(flag == 2):
+        string3=re.compile('[A-Za-z]+[-](\d+[.]\d+)')
+        y=string3.findall(expr)
+        string2=re.compile('[-]?\d+[.]\d+')
+        x=string2.findall(expr)
+        a['type']="sigmoid"
+        a['rate']  = x[0]
+        if(len(y) == 0):
+            a['midpoint']=x[1]
+        else:
+            a['midpoint']=y[0]
+        a['scale']=x[2]
+    if(flag == 3):
+        string3=re.compile('[A-Za-z]+[-](\d+[.]\d+)')
+        y=string3.findall(expr)
+        string2=re.compile('[-]?\d+[.]\d+')
+        x=string2.findall(expr)
+        if(x[1] == x[3] and x[2] == x[4]):
+            a['type'] = "exp_linear"
+            a['rate'] = x[0]
+            if(len(y) == 0):
+                a['midpoint']=x[1]
+            else:
+                a['midpoint']=y[0]
+            a['scale'] = x[2]
+        else:
+            a['type'] = "generic"
+    if(flag == 4):
+        a['type'] = "generic"
+    
+    return a
+    
+         
+    return expr
 
 def raiseLexicalError(err):
     import sys
